8cc58960f7d8f20ffc0742af80436e18a60af918
scherepanov <sergey@cherepanov.org.ua>
2022-01-02 23:37:05 +0400

    mutagen support

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -4,20 +4,9 @@ set -e
 
 cd /var/www
 
-mkdir -p node_modules
-chown linuxbrew:linuxbrew node_modules 2> /dev/null || true
-
-mkdir -p vendor
-chown linuxbrew:linuxbrew vendor 2> /dev/null || true
-
-mkdir -p /var/www/.composer
-chown linuxbrew:linuxbrew /var/www/.composer 2> /dev/null || true
-
-mkdir -p /var/www/.npm
-chown linuxbrew:linuxbrew /var/www/.npm 2> /dev/null || true
-
-mkdir -p /var/www/var/cache
-chown linuxbrew:linuxbrew /var/www/var/cache 2> /dev/null || true
+chown linuxbrew:linuxbrew /var/www 2> /dev/null || true
+chown linuxbrew:linuxbrew /home/linuxbrew/.composer 2> /dev/null || true
+chown linuxbrew:linuxbrew /home/linuxbrew/.npm 2> /dev/null || true
 
 if [[ -f /var/www/config/parameters.yml ]]; then
   sed -i 's/database_driver:.*/database_driver: '${ORO_DB_DRIVER}'/g' /var/www/config/parameters.yml
