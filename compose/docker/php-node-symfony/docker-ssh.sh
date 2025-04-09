adf8e555b4585d7d08f8b197083cae5243482f94
scherepanov <s@cherepanov.co>
2025-04-09 15:36:03 +0400

    v6

--- compose/docker/php-node-symfony/docker-ssh.sh
+++ compose/docker/php-node-symfony/docker-ssh.sh
@@ -3,8 +3,8 @@ set -ex
 [ -f /etc/ssh/hostkeys/ssh_host_rsa_key ] || ssh-keygen -t rsa -f /etc/ssh/hostkeys/ssh_host_rsa_key -N ''; \
 [ -f /etc/ssh/hostkeys/ssh_host_ecdsa_key ] || ssh-keygen -t ecdsa -f /etc/ssh/hostkeys/ssh_host_ecdsa_key -N ''; \
 [ -f /etc/ssh/hostkeys/ssh_host_ed25519_key ] || ssh-keygen -t ed25519 -f /etc/ssh/hostkeys/ssh_host_ed25519_key -N ''; \
-sed -i 's|^HostKey .*rsa.*|HostKey /etc/ssh/hostkeys/ssh_host_rsa_key|' /etc/ssh/sshd_config; \
-sed -i 's|^HostKey .*ecdsa.*|HostKey /etc/ssh/hostkeys/ssh_host_ecdsa_key|' /etc/ssh/sshd_config; \
-sed -i 's|^HostKey .*ed25519.*|HostKey /etc/ssh/hostkeys/ssh_host_ed25519_key|' /etc/ssh/sshd_config; \
+sed -i '/^#HostKey \/etc\/ssh\/ssh_host_rsa_key/a HostKey /etc/ssh/hostkeys/ssh_host_rsa_key' /etc/ssh/sshd_config
+sed -i '/^#HostKey \/etc\/ssh\/ssh_host_ecdsa_key/a HostKey /etc/ssh/hostkeys/ssh_host_ecdsa_key' /etc/ssh/sshd_config
+sed -i '/^#HostKey \/etc\/ssh\/ssh_host_ed25519_key/a HostKey /etc/ssh/hostkeys/ssh_host_ed25519_key' /etc/ssh/sshd_config
 
 exec /usr/sbin/sshd
