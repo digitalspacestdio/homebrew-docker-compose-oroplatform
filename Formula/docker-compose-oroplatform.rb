5cd6c15c3ab988b407ece20e926b8e44faff06e4
scherepanov <s@cherepanov.co>
2025-04-09 14:45:32 +0400

    modified:   Formula/docker-compose-oroplatform.rb

--- Formula/docker-compose-oroplatform.rb
+++ Formula/docker-compose-oroplatform.rb
@@ -3,7 +3,7 @@ require 'formula'
 class DockerComposeOroplatform < Formula
   url "https://github.com/digitalspacestdio/homebrew-docker-compose-oroplatform.git", :using => :git
   version "0.6.0"
-  revision 5
+  revision 6
 
   depends_on 'coreutils'
   depends_on 'rsync'
