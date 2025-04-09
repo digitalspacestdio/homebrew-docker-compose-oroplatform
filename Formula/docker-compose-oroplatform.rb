39094f0b013d55fd8cfd93fd1be741677fe7dc64
scherepanov <s@cherepanov.co>
2025-04-09 16:06:50 +0400

    v6

--- Formula/docker-compose-oroplatform.rb
+++ Formula/docker-compose-oroplatform.rb
@@ -3,7 +3,7 @@ require 'formula'
 class DockerComposeOroplatform < Formula
   url "https://github.com/digitalspacestdio/homebrew-docker-compose-oroplatform.git", :using => :git
   version "0.6.0"
-  revision 21
+  revision 22
 
   depends_on 'coreutils'
   depends_on 'rsync'
--- compose/docker/php-node-symfony/docker-ssh.sh
+++ compose/docker/php-node-symfony/docker-ssh.sh
@@ -7,4 +7,17 @@ sed -i '/^#HostKey \/etc\/ssh\/ssh_host_rsa_key/a HostKey /etc/ssh/hostkeys/ssh_
 sed -i '/^#HostKey \/etc\/ssh\/ssh_host_ecdsa_key/a HostKey /etc/ssh/hostkeys/ssh_host_ecdsa_key' /etc/ssh/sshd_config
 sed -i '/^#HostKey \/etc\/ssh\/ssh_host_ed25519_key/a HostKey /etc/ssh/hostkeys/ssh_host_ed25519_key' /etc/ssh/sshd_config
 
+if [[ -n $ORO_SSH_PUBLIC_KEY ]]; then
+	mkdir -p /root/.ssh
+	echo "${ORO_SSH_PUBLIC_KEY}" > /root/.ssh/authorized_keys
+	chmod -R 0600 /root/.ssh
+
+	if [[ -n $PHP_UID ]]; then
+		PHP_USER_HOME=$(eval echo "~$PHP_USER_NAME")
+		mkdir -p ${PHP_USER_HOME}/.ssh
+		echo "${ORO_SSH_PUBLIC_KEY}" > ${PHP_USER_HOME}/.ssh/authorized_keys
+		chmod -R 0600 ${PHP_USER_HOME}/.ssh
+	fi
+fi
+
 exec /usr/sbin/sshd -D
