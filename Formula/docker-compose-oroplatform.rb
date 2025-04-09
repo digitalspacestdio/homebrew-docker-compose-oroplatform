d47707df6120ade67e156406f9340ef2ba058503
scherepanov <s@cherepanov.co>
2025-04-09 14:43:47 +0400

    modified:   Formula/docker-compose-oroplatform.rb

--- Formula/docker-compose-oroplatform.rb
+++ Formula/docker-compose-oroplatform.rb
@@ -3,14 +3,15 @@ require 'formula'
 class DockerComposeOroplatform < Formula
   url "https://github.com/digitalspacestdio/homebrew-docker-compose-oroplatform.git", :using => :git
   version "0.6.0"
-  revision 4
+  revision 5
 
   depends_on 'coreutils'
   depends_on 'rsync'
   depends_on 'mutagen-io/mutagen/mutagen' if OS.mac?
 
   def install
-    bin.install "bin/dc-oro"
+    libexec.install "bin/dc-oro"
+    bin.write_exec_script libexec/"bin/dc-oro"
 
     pkgshare.install "compose"
   end
