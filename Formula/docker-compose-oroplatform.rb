05edaf6694032d49824abfe6a3f8976fa3e866c5
scherepanov <s@cherepanov.co>
2025-04-24 12:57:31 +0400

    fix dns and port functions

--- Formula/docker-compose-oroplatform.rb
+++ Formula/docker-compose-oroplatform.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class DockerComposeOroplatform < Formula
   url "https://github.com/digitalspacestdio/homebrew-docker-compose-oroplatform.git", :using => :git
-  version "0.7.4"
-  revision 6
+  version "0.7.5"
+  revision 1
 
   depends_on 'yq'
   depends_on 'jq'
