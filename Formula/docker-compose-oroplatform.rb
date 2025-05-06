6eb24e8f236cebedc7aac5293de7d45b8c5e9ac3
scherepanov <s@cherepanov.co>
2025-05-07 01:25:03 +0400

    shm sidecar

--- Formula/docker-compose-oroplatform.rb
+++ Formula/docker-compose-oroplatform.rb
@@ -3,7 +3,7 @@ require 'formula'
 class DockerComposeOroplatform < Formula
   url "https://github.com/digitalspacestdio/homebrew-docker-compose-oroplatform.git", :using => :git
   version "0.7.15"
-  revision 6
+  revision 8
 
   depends_on 'yq'
   depends_on 'jq'
