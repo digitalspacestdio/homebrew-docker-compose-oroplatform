c671cd88a60fc9fdee0e3d89277280a5066756f8
scherepanov <s@cherepanov.co>
2025-06-03 12:48:34 +0400

    improve free port resolution

--- Formula/docker-compose-oroplatform.rb
+++ Formula/docker-compose-oroplatform.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class DockerComposeOroplatform < Formula
   url "https://github.com/digitalspacestdio/homebrew-docker-compose-oroplatform.git", :using => :git
-  version "0.7.21"
+  version "0.7.22"
   revision 1
 
   depends_on 'yq'
