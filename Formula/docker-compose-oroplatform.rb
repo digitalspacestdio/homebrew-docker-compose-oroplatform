1e7f7810ded07248f7fcca7c9fbd8d8d36a3601c
scherepanov <s@cherepanov.co>
2025-07-18 21:02:53 +0400

    importdb fixes

--- Formula/docker-compose-oroplatform.rb
+++ Formula/docker-compose-oroplatform.rb
@@ -3,7 +3,7 @@ require 'formula'
 class DockerComposeOroplatform < Formula
   url "https://github.com/digitalspacestdio/homebrew-docker-compose-oroplatform.git", :using => :git
   version "0.7.23"
-  revision 8
+  revision 9
 
   depends_on 'yq'
   depends_on 'jq'
